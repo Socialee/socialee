b1940ddfc510a107e879b47da2a143f8b2415ffa
Daniel Hahler <git@thequod.de>
2015-04-29 08:22:54 +0200

    config/urls: cleanup (new style patterns); style fixes

--- config/urls.py
+++ config/urls.py
@@ -1,15 +1,18 @@
 from django.conf import settings
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
-admin.autodiscover()
 
-urlpatterns = patterns('',
-    url(r'^$', 'socialee.views.home', name='home'),
+import socialee
+
+urlpatterns = [
+    url(r'^$', socialee.views.home, name='home'),
 
     url(r'^admin/', include(admin.site.urls)),
-)
+]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
