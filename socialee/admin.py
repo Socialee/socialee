5f902076845e737d8d1edf2df5b4bee2e31ca6be
Moritz Jüdes <mail@moritzjuedes.de>
2015-05-21 04:19:33 +0200

    500 error fixed, search field fixed

--- socialee/admin.py
+++ socialee/admin.py
@@ -58,7 +58,7 @@ class ProfileAdmin(admin.ModelAdmin):
 
 
 class ProfileErfassungsAdmin(admin.ModelAdmin):
-    search_fields = ['user', 'email', 'lastname', 'firstname']
+    search_fields = ['email', 'lastname', 'firstname']
     list_display = ['__str__', 'firstname', 'lastname', 'email']
     inlines = [
         InputProfileInline, OutputProfileInline
