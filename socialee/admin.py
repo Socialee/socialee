e74b1355d4f38be4a1942bdf94f1b44f7e8e4d3f
Daniel Hahler <git@thequod.de>
2015-05-21 03:18:08 +0200

    admin: Location, Origin, Input/Output-Refactoring

--- socialee/admin.py
+++ socialee/admin.py
@@ -1,14 +1,31 @@
 from django.contrib import admin
 
-from .models import Input, Output, Profile, Zettel
+from .models import Input, Output, Origin, Profile, Zettel
+from .models import ProfileErfassung
 
 
-class InputInline(admin.TabularInline):
+class InputZettelInline(admin.TabularInline):
     model = Input
+    exclude = ('profile',)
 
 
-class OutputInline(admin.TabularInline):
+class OutputZettelInline(admin.TabularInline):
     model = Output
+    exclude = ('profile',)
+
+
+class InputProfileInline(admin.TabularInline):
+    model = Input
+    exclude = ('zettel',)
+
+
+class OutputProfileInline(admin.TabularInline):
+    model = Output
+    exclude = ('zettel',)
+
+
+class ProfileInline(admin.TabularInline):
+    model = Profile
 
 
 class ZettelInline(admin.TabularInline):
@@ -27,17 +44,30 @@ class OutputAdmin(admin.ModelAdmin):
 
 class ZettelAdmin(admin.ModelAdmin):
     model = Zettel
+    inlines = [
+        InputZettelInline, OutputZettelInline
+    ]
+
+
+class OriginAdmin(admin.ModelAdmin):
+    model = Origin
 
 
 class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+class ProfileErfassungsAdmin(admin.ModelAdmin):
     search_fields = ['user', 'email', 'lastname', 'firstname']
     list_display = ['__str__', 'firstname', 'lastname', 'email']
     inlines = [
-        InputInline, OutputInline, ZettelInline
+        InputProfileInline, OutputProfileInline
     ]
 
 
 admin.site.register(Input, InputAdmin)
 admin.site.register(Output, OutputAdmin)
+admin.site.register(Origin, OriginAdmin)
 admin.site.register(Profile, ProfileAdmin)
+admin.site.register(ProfileErfassung, ProfileErfassungsAdmin)
 admin.site.register(Zettel, ZettelAdmin)
