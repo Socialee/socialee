435202be4c8d34758626793a6f72851d57f6863c
Daniel Hahler <git@thequod.de>
2015-06-06 12:11:20 +0200

    Add basic tests for admin

---
+++ tests/test_admin.py
@@ -0,0 +1,22 @@
+from django.core.urlresolvers import reverse
+
+
+def test_admin_index(admin_client):
+    response = admin_client.get(reverse('admin:index'))
+    assert response.status_code == 200
+
+def test_userentry_list(admin_client):
+    response = admin_client.get(reverse('admin:socialee_userentry_changelist'))
+    assert response.status_code == 200
+
+def test_userentry_add(admin_client):
+    response = admin_client.get(reverse('admin:socialee_userentry_add'))
+    assert response.status_code == 200
+
+def test_profileentry_list(admin_client):
+    response = admin_client.get(reverse('admin:socialee_profileerfassung_changelist'))
+    assert response.status_code == 200
+
+def test_profileentry_add(admin_client):
+    response = admin_client.get(reverse('admin:socialee_profileerfassung_add'))
+    assert response.status_code == 200
