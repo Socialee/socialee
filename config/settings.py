d08eb7368699fec23c388c1839a7dff4462366f3
Daniel Hahler <git@thequod.de>
2015-07-03 00:05:55 +0200

    settings: LANGUAGE_CODE=de

--- config/settings.py
+++ config/settings.py
@@ -149,13 +149,13 @@ MIGRATION_MODULES = {
 # Internationalization
 # https://docs.djangoproject.com/en/dev/topics/i18n/
 
-LANGUAGE_CODE = 'de-de'
+LANGUAGE_CODE = 'de'
 TIME_ZONE = 'Europe/Berlin'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 LANGUAGES = [
-    ('de-de', 'German'),
+    ('de', 'German'),
 ]
 
 
