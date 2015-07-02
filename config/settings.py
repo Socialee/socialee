02b6d4079dd2da444c90fac7be5e3078effb3b4b
Moritz Jüdes <mail@moritzjuedes.de>
2015-07-02 19:47:48 +0200

    Install Django-CMS

--- config/settings.py
+++ config/settings.py
@@ -10,13 +10,26 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 TODO: use django-configurations, for class based approach?!
 """
 
+# -*- coding: utf-8 -*-
 import os
+gettext = lambda s: s
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 import environ
 
 ROOT_DIR = environ.Path(__file__) - 2
 APPS_DIR = ROOT_DIR.path('socialee')
 assert os.path.exists(str(ROOT_DIR.path("Makefile"))), "ROOT_DIR is set properly."
+
+# TEMPLATE_DIRS = (
+#     # The docs say it should be absolute path: BASE_DIR is precisely one.
+#     # Life is wonderful!
+#     os.path.join(BASE_DIR, "templates"),
+# )
+
+CMS_TEMPLATES = (
+    ('template_1.html', 'Template One'),
+)
 
 env = environ.Env()
 
@@ -49,6 +62,24 @@ INSTALLED_APPS = (
     'django.contrib.sites',
 
     'socialee',
+
+    'cms',  # django CMS itself
+    'mptt',  # utilities for implementing a tree
+    'menus',  # helper for model independent hierarchical website navigation
+    'sekizai',  # for javascript and css management
+    'djangocms_admin_style',  # for the admin skin. You **must** add 'djangocms_admin_style' in the list **before** 'django.contrib.admin'.
+    'treebeard'
+
+    # 'reversion'
+    # 'djangocms_file',
+    # 'djangocms_flash',
+    # 'djangocms_googlemap',
+    # 'djangocms_inherit',
+    # 'djangocms_picture',
+    # 'djangocms_teaser',
+    # 'djangocms_video',
+    # 'djangocms_link',
+    # 'djangocms_snippet',
 )
 
 if DEBUG:
@@ -66,6 +97,12 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'cms.middleware.user.CurrentUserMiddleware',
+    'cms.middleware.page.CurrentPageMiddleware',
+    'cms.middleware.toolbar.ToolbarMiddleware',
+    'cms.middleware.language.LanguageCookieMiddleware',
+    'django.contrib.admindocs.middleware.XViewMiddleware',
 )
 
 ROOT_URLCONF = 'config.urls'
@@ -81,6 +118,11 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.core.context_processors.i18n',
+                'django.core.context_processors.media',
+                'django.core.context_processors.static',
+                'sekizai.context_processors.sekizai',
+                'cms.context_processors.cms_settings',
             ],
         },
     },
@@ -96,15 +138,35 @@ DATABASES = {
 }
 DATABASES['default']['ATOMIC_REQUESTS'] = True
 
+MIGRATION_MODULES = {
+    'cms': 'cms.migrations_django',
+    'menus': 'menus.migrations_django',
+
+    # Add also the following modules if you're using these plugins:
+    'djangocms_file': 'djangocms_file.migrations_django',
+    'djangocms_flash': 'djangocms_flash.migrations_django',
+    'djangocms_googlemap': 'djangocms_googlemap.migrations_django',
+    'djangocms_inherit': 'djangocms_inherit.migrations_django',
+    'djangocms_link': 'djangocms_link.migrations_django',
+    'djangocms_picture': 'djangocms_picture.migrations_django',
+    'djangocms_snippet': 'djangocms_snippet.migrations_django',
+    'djangocms_teaser': 'djangocms_teaser.migrations_django',
+    'djangocms_video': 'djangocms_video.migrations_django',
+    'djangocms_text_ckeditor': 'djangocms_text_ckeditor.migrations_django',
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/dev/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
 TIME_ZONE = 'Europe/Berlin'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
+
+# LANGUAGES = [
+#     ('en', 'English'),
+# ]
 
 
 # Static files (CSS, JavaScript, Images)
--- config/urls.py
+++ config/urls.py
@@ -1,18 +1,27 @@
 from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 
 from socialee import views
 
-urlpatterns = [
+urlpatterns = i18n_patterns('',
+    url(r'^', include('cms.urls')),
     url(r'^$', views.home, name='home'),
-
     url(r'^admin/', include(admin.site.urls)),
-]
+)
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL,
                           document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
+
+
+# und so steht's in den cms-docs:
+
+# urlpatterns = i18n_patterns('',
+#     url(r'^admin/', include(admin.site.urls)),
+#     url(r'^', include('cms.urls')),
+# ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
