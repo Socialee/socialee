8b81729061dfee476c71ed547b810dd87b504e5a
Daniel Hahler <git@thequod.de>
2015-07-12 02:49:18 +0200

    Fix InvalidBasesError error, because of invalid MIGRATION_MODULES
    Ref: https://code.djangoproject.com/ticket/25109
    
    Fixes https://github.com/Socialee/socialee/issues/23

--- config/settings.py
+++ config/settings.py
@@ -178,7 +178,6 @@ MIGRATION_MODULES = {
     'djangocms_snippet': 'djangocms_snippet.migrations_django',
     'djangocms_teaser': 'djangocms_teaser.migrations_django',
     'djangocms_video': 'djangocms_video.migrations_django',
-    'djangocms_text_ckeditor': 'djangocms_text_ckeditor.migrations_django',
 }
 
 # Internationalization
