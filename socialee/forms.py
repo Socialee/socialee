1c672a1194b5f9cf45cf68e2549c82bacd3f71a9
Daniel Hahler <git@thequod.de>
2015-08-27 23:28:56 +0200

    minor: tabs to spaces

--- socialee/forms.py
+++ socialee/forms.py
@@ -5,73 +5,73 @@ from .models import Project, Profile, UserEntry, Wish, Dream, Input, Output
 
 
 class MySignupForm(SignupForm):
-	"A specialized signup form to not require the password fields."
-	def __init__(self, *args, **kwargs):
-		super(MySignupForm, self).__init__(*args, **kwargs)
-		del self.fields["password1"]
-		del self.fields["password2"]
+    "A specialized signup form to not require the password fields."
+    def __init__(self, *args, **kwargs):
+        super(MySignupForm, self).__init__(*args, **kwargs)
+        del self.fields["password1"]
+        del self.fields["password2"]
 
-	dream_title = forms.CharField(label='Wenn es an nichts fehlen würde, nicht an Geld, nicht an Zeit, was würdest Du tun? Wofür brennst Du?', widget=forms.Textarea(attrs={'placeholder': 'Neil und Moritz würden mit einem Trecker quer durch Deutschland fahren. Socialee würde eine Webseite bauen, die diese Frage stellt.','rows':4}), required=False, max_length=5000)
-	wish_title = forms.CharField(label='Du hast 5000 Zeichen. Lass Dich ruhig aus.', widget=forms.Textarea(attrs={'placeholder': '','rows':4}), required=False, max_length=5000)
-	project_title = forms.CharField(label='Socialee sammelt Ideen und Projekte. Und wir bauen ein Netzwerk drumrum und dazwischen.', widget=forms.Textarea(attrs={'placeholder': 'Corinna arbeitet mit Flüchtlingen, Waldemar produziert tolle Kondome. Socialee sagt: "Stellt euch mal ein soziales Netzwerk vor!"','rows':4}), required=False, max_length=5000)	
-	output_title = forms.CharField(label='Und, was fehlt Dir? Welches Bedürfnis wird nicht befriedigt?', widget=forms.Textarea(attrs={'placeholder': 'Susi fehlt ein Fahrrad. Helge fehlt eine Freundin. Socialee fehlt auch noch alles Mögliche. ','rows':4}), required=False, max_length=5000)	
-	input_title= forms.CharField(label='Wenn wir uns mal kurz entspannen, fällt uns wieder ein, was wir so alles gut können. Uh, wie gut Du bist!', widget=forms.Textarea(attrs={'placeholder': 'Petra kann Autos reparieren und Peter arbeitet gerne mit Kindern. Fadhumo kann Flüchtlinge motivieren und Socialee kann ganz gut zuhören.','rows':4}), required=False, max_length=5000)	
+    dream_title = forms.CharField(label='Wenn es an nichts fehlen würde, nicht an Geld, nicht an Zeit, was würdest Du tun? Wofür brennst Du?', widget=forms.Textarea(attrs={'placeholder': 'Neil und Moritz würden mit einem Trecker quer durch Deutschland fahren. Socialee würde eine Webseite bauen, die diese Frage stellt.','rows':4}), required=False, max_length=5000)
+    wish_title = forms.CharField(label='Du hast 5000 Zeichen. Lass Dich ruhig aus.', widget=forms.Textarea(attrs={'placeholder': '','rows':4}), required=False, max_length=5000)
+    project_title = forms.CharField(label='Socialee sammelt Ideen und Projekte. Und wir bauen ein Netzwerk drumrum und dazwischen.', widget=forms.Textarea(attrs={'placeholder': 'Corinna arbeitet mit Flüchtlingen, Waldemar produziert tolle Kondome. Socialee sagt: "Stellt euch mal ein soziales Netzwerk vor!"','rows':4}), required=False, max_length=5000)   
+    output_title = forms.CharField(label='Und, was fehlt Dir? Welches Bedürfnis wird nicht befriedigt?', widget=forms.Textarea(attrs={'placeholder': 'Susi fehlt ein Fahrrad. Helge fehlt eine Freundin. Socialee fehlt auch noch alles Mögliche. ','rows':4}), required=False, max_length=5000)    
+    input_title= forms.CharField(label='Wenn wir uns mal kurz entspannen, fällt uns wieder ein, was wir so alles gut können. Uh, wie gut Du bist!', widget=forms.Textarea(attrs={'placeholder': 'Petra kann Autos reparieren und Peter arbeitet gerne mit Kindern. Fadhumo kann Flüchtlinge motivieren und Socialee kann ganz gut zuhören.','rows':4}), required=False, max_length=5000)    
 
-	first_name = forms.CharField(label='Vorname', widget=forms.TextInput(attrs={'placeholder': 'Vorname'}), max_length=30)
-	last_name = forms.CharField(label='Nachname', widget=forms.TextInput(attrs={'placeholder': 'Nachname'}), max_length=30)
-	# newsletter = forms.BooleanField()
+    first_name = forms.CharField(label='Vorname', widget=forms.TextInput(attrs={'placeholder': 'Vorname'}), max_length=30)
+    last_name = forms.CharField(label='Nachname', widget=forms.TextInput(attrs={'placeholder': 'Nachname'}), max_length=30)
+    # newsletter = forms.BooleanField()
 
 
-	def save(self, request):
-		user = super(MySignupForm, self).save(request)
+    def save(self, request):
+        user = super(MySignupForm, self).save(request)
 
-		user.first_name = self.cleaned_data['first_name']
-		user.last_name = self.cleaned_data['last_name']
-		user.save()
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.save()
 
-		# HACK
-		user = UserEntry(id=user.id)
+        # HACK
+        user = UserEntry(id=user.id)
 
-		profile = Profile.objects.create(user=user)
+        profile = Profile.objects.create(user=user)
 
-		if self.cleaned_data['project_title']:
-			project = Project.objects.create(title=self.cleaned_data['project_title'])
-			project.profiles.add(profile)
-			project.save()
+        if self.cleaned_data['project_title']:
+            project = Project.objects.create(title=self.cleaned_data['project_title'])
+            project.profiles.add(profile)
+            project.save()
 
-		if self.cleaned_data['wish_title']:
-			wish = Wish.objects.create(title=self.cleaned_data['wish_title'])
-			wish.profiles.add(profile)
-			wish.save()
+        if self.cleaned_data['wish_title']:
+            wish = Wish.objects.create(title=self.cleaned_data['wish_title'])
+            wish.profiles.add(profile)
+            wish.save()
 
-		if self.cleaned_data['dream_title']:
-			dream = Dream.objects.create(title=self.cleaned_data['dream_title'])
-			dream.profiles.add(profile)
-			dream.save()
+        if self.cleaned_data['dream_title']:
+            dream = Dream.objects.create(title=self.cleaned_data['dream_title'])
+            dream.profiles.add(profile)
+            dream.save()
 
-		if self.cleaned_data['input_title']:
-			input = Input.objects.create(title=self.cleaned_data['input_title'])
-			input.profiles.add(profile)
-			input.save()
+        if self.cleaned_data['input_title']:
+            input = Input.objects.create(title=self.cleaned_data['input_title'])
+            input.profiles.add(profile)
+            input.save()
 
-		if self.cleaned_data['output_title']:
-			output = Output.objects.create(title=self.cleaned_data['output_title'])
-			output.profiles.add(profile)
-			output.save()
+        if self.cleaned_data['output_title']:
+            output = Output.objects.create(title=self.cleaned_data['output_title'])
+            output.profiles.add(profile)
+            output.save()
 
 
 # class ProjectForm(forms.ModelForm):
-# 	class Meta:
-# 		model = Project
-# 		fields = ['title']
+#   class Meta:
+#       model = Project
+#       fields = ['title']
 
 #class DreamForm(forms.ModelForm):
-#	class Meta:
-#		model = Dream
-#		fields = ['title']
+#   class Meta:
+#       model = Dream
+#       fields = ['title']
 
 
 #class WishForm(forms.ModelForm):
-#	class Meta:
-#		model = Wish
-#		fields = ['title']
+#   class Meta:
+#       model = Wish
+#       fields = ['title']
