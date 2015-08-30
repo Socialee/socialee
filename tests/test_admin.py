d9728d7f8b41cad85b0f314b62a70d74314815c1
Daniel Hahler <git@thequod.de>
2015-08-31 00:43:06 +0200

    fixup! tests: follow=True for SECURE_SSL_REDIRECT on Heroku

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -5,18 +5,22 @@ def test_admin_index(admin_client):
     response = admin_client.get(reverse('admin:index'), follow=True)
     assert response.status_code == 200
 
-def test_userentry_list(admin_client, follow=True):
-    response = admin_client.get(reverse('admin:socialee_userentry_changelist'))
+def test_userentry_list(admin_client):
+    response = admin_client.get(reverse('admin:socialee_userentry_changelist'),
+                                follow=True)
     assert response.status_code == 200
 
-def test_userentry_add(admin_client, follow=True):
-    response = admin_client.get(reverse('admin:socialee_userentry_add'))
+def test_userentry_add(admin_client):
+    response = admin_client.get(reverse('admin:socialee_userentry_add'),
+                                follow=True)
     assert response.status_code == 200
 
-def test_profileentry_list(admin_client, follow=True):
-    response = admin_client.get(reverse('admin:socialee_profile_changelist'))
+def test_profileentry_list(admin_client):
+    response = admin_client.get(reverse('admin:socialee_profile_changelist'),
+                                follow=True)
     assert response.status_code == 200
 
-def test_profileentry_add(admin_client, follow=True):
-    response = admin_client.get(reverse('admin:socialee_profile_add'))
+def test_profileentry_add(admin_client):
+    response = admin_client.get(reverse('admin:socialee_profile_add'),
+                                follow=True)
     assert response.status_code == 200
