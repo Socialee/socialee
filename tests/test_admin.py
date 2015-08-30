02e4b68688df92ba634391037ee9738749f124c4
Daniel Hahler <git@thequod.de>
2015-08-31 00:25:21 +0200

    tests: follow=True for SECURE_SSL_REDIRECT on Heroku

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -2,21 +2,21 @@ from django.core.urlresolvers import reverse
 
 
 def test_admin_index(admin_client):
-    response = admin_client.get(reverse('admin:index'))
+    response = admin_client.get(reverse('admin:index'), follow=True)
     assert response.status_code == 200
 
-def test_userentry_list(admin_client):
+def test_userentry_list(admin_client, follow=True):
     response = admin_client.get(reverse('admin:socialee_userentry_changelist'))
     assert response.status_code == 200
 
-def test_userentry_add(admin_client):
+def test_userentry_add(admin_client, follow=True):
     response = admin_client.get(reverse('admin:socialee_userentry_add'))
     assert response.status_code == 200
 
-def test_profileentry_list(admin_client):
+def test_profileentry_list(admin_client, follow=True):
     response = admin_client.get(reverse('admin:socialee_profile_changelist'))
     assert response.status_code == 200
 
-def test_profileentry_add(admin_client):
+def test_profileentry_add(admin_client, follow=True):
     response = admin_client.get(reverse('admin:socialee_profile_add'))
     assert response.status_code == 200
