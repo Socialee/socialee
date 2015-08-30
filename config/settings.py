70d7b3e8f4c07a522c2236d8f860ca7deab47d70
Daniel Hahler <git@thequod.de>
2015-08-30 23:08:21 +0200

    Add django-sslify for Heroku

--- config/settings.py
+++ config/settings.py
@@ -233,6 +233,12 @@ MEDIA_ROOT = str(ROOT_DIR('media'))
 STATIC_ROOT = str(ROOT_DIR('build', 'static'))
 STATICFILES_DIRS = (str(APPS_DIR('static')), )
 
+# Add django-sslify on Heroku, based on env setting.
+# > heroku config:set --app socialee-staging DJANGO_SECURE_PROXY_SSL_HEADER=HTTP_X_FORWARDED_PROTO,https
+SECURE_PROXY_SSL_HEADER = tuple(env.list('DJANGO_SECURE_PROXY_SSL_HEADER', []))
+if SECURE_PROXY_SSL_HEADER:
+    MIDDLEWARE_CLASSES = ['sslify.middleware.SSLifyMiddleware'] + \
+        MIDDLEWARE_CLASSES
 
 # Configure logging, especially for Heroku.
 LOGGING = {
