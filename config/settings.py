535f5571d32cc7a8c42f2a5d200bf62c8bd0aafe
Daniel Hahler <git@thequod.de>
2016-04-13 12:19:32 +0200

    settings: remove MIGRATION_MODULES, which is not necessary with dj19

--- config/settings.py
+++ config/settings.py
@@ -175,19 +175,6 @@ DATABASES = {
 }
 DATABASES['default']['ATOMIC_REQUESTS'] = True
 
-MIGRATION_MODULES = {
-    # For non-ported Django-CMS plugins.
-    # Ref: https://django-cms.readthedocs.org/en/latest/how_to/install.html
-    'djangocms_file': 'djangocms_file.migrations_django',
-    'djangocms_flash': 'djangocms_flash.migrations_django',
-    'djangocms_googlemap': 'djangocms_googlemap.migrations_django',
-    'djangocms_inherit': 'djangocms_inherit.migrations_django',
-    'djangocms_link': 'djangocms_link.migrations_django',
-    'djangocms_picture': 'djangocms_picture.migrations_django',
-    'djangocms_teaser': 'djangocms_teaser.migrations_django',
-    'djangocms_video': 'djangocms_video.migrations_django',
-}
-
 # Internationalization
 # https://docs.djangoproject.com/en/dev/topics/i18n/
 
