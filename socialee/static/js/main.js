8632720d5d2ace9899255f8a95d5627bd38da954
Moritz Juedes <mail@moritzjuedes.de>
2016-08-26 19:47:52 +0200

    Add Link to Blog to Landingpage

--- socialee/static/js/main.js
+++ socialee/static/js/main.js
@@ -3,6 +3,22 @@
 // Foundation
 $(document).foundation();
 
+$(document).ready(function(){
+
+  $('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 1200, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+});
+
 // using jQuery
 function getCookie(name) {
     var cookieValue = null;
