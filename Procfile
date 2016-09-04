8b2e4fccec4e778210b886ac885e524e5dfe1201
Moritz Juedes <mail@moritzjuedes.de>
2016-09-04 12:21:11 +0200

    Cleanup the mess I made today

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn config.wsgi:application --log-file -
+web: gunicorn config.wsgi:application_with_static --log-file -
--- config/wsgi.py
+++ config/wsgi.py
@@ -14,9 +14,9 @@ from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
 
-# For Heroku. (old settings with dj-static, also see: Procfile in root-folder!)
-# try:
-#     from dj_static import Cling, MediaCling
-#     application_with_static = Cling(MediaCling(application))
-# except ImportError:
-#     pass
+# For Heroku.
+try:
+    from dj_static import Cling, MediaCling
+    application_with_static = Cling(MediaCling(application))
+except ImportError:
+    pass
