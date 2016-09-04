aeb9a80cd67db9984abc19e21277a6f3b198955a
Moritz Juedes <mail@moritzjuedes.de>
2016-09-04 11:36:15 +0200

    Disable whitenoise (as it keeps throwing errors) and change Procfile

--- config/wsgi.py
+++ config/wsgi.py
@@ -13,5 +13,10 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-# For Heroku
-application_with_static = get_wsgi_application()
+
+# For Heroku. (old settings with dj-static, also see: Procfile in root-folder!)
+# try:
+#     from dj_static import Cling, MediaCling
+#     application_with_static = Cling(MediaCling(application))
+# except ImportError:
+#     pass
