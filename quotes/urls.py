72152a93992edebf35b4e558ba905506e20e4791
Moritz Juedes <mail@moritzjuedes.de>
2016-09-14 00:15:45 +0200

    Small Fixes for Quotes and styles

--- quotes/urls.py
+++ quotes/urls.py
@@ -4,5 +4,4 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.create_quote, name='make_quote'),
-    url(r'^show-all-quotes/$', views.show_all_quotes, name='show_quote'),
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -15,24 +15,9 @@ def create_quote(request):
         instance.save()
         messages.success(request, 'Danke! Wir gucken uns Dein Zitat an und veröffentlichen es, sofern es jugendfrei ist.')
         return HttpResponseRedirect('/')
-
-    # else: # TODO: Gibt zwei mal error zurück, wenn form nicht ausgefüllt wird. (zb. bei raus hier)
-    #   messages.error(request, 'Oh, da ist was schiefgelaufen.')
     
     context = {
         "form": form,
     }
     
     return render(request, "create_quote.html", context)
-
-
-def show_all_quotes(request):
-    quotes = Quote.objects.filter(active=True)
-    
-    if quotes:
-        all_active_quotes = list(Quote.objects.filter(active=True))
-        context = {
-        "all_quotes": all_active_quotes
-        }
-
-        return render(request, "show-all-quotes.html", context)
