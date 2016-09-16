2240560f1f2de3a7a091f32f6b812bbb2e00ce74
Moritz Juedes <mail@moritzjuedes.de>
2016-09-16 23:08:37 +0200

    Claenup urls.py reorder and give IDs to url patterns

--- config/urls.py
+++ config/urls.py
@@ -11,57 +11,66 @@ from . import allauth_urls
 if settings.DEBUG or settings.SIMPLE_AUTH:
     urlpatterns = [
         
-        url(r'^$', views.Home.as_view(), name='home'),
-        url(r'^accounts/', include('allauth.urls')),
-        url(r'^admin/', include(admin.site.urls)),
-        url(r'^summernote/', include('django_summernote.urls')),
-        url(r'^question/', include('questions.urls')),
-        url(r'^quotes/', include('quotes.urls')),
-        url(r'^blog/', include('zinnia.urls')),
-        url(r'^comments/', include('django_comments.urls')),
-        url(r'^protect/', include('simple_auth.urls')),
-        url(r'^invite_me/$', views.NewsletterSignup.as_view(), name='invite_me'),
-        url(r'^impressum/$', views.Impressum.as_view(), name='impressum'),
-        url(r'^welcome/(?P<slug>[\w.@+-]+)/$', login_required(views.WelcomePage.as_view()), name='welcome'), # login required!
-        url(r'^startproject/$', login_required(views.StartProject.as_view()), name='startproject'), # login required!
-        url(r'^project_overview/$', views.ProjectOverview.as_view(), name='project_overview'),
-        url(r'^project/(?P<slug>[-\w]+)/$', login_required(views.ProjectDetailView.as_view()), name='project_detailview'),
-        url(r'^project/(?P<slug>[-\w]+)/edit/$', login_required(views.ProjectUpdateView.as_view()), name='project_updateview'),
-        url(r'^profile/(?P<slug>[\w.@+-]+)/profile/update/$', login_required(views.ProfileUpdateView.as_view()), name='update_profile'),
-        url(r'^profile/(?P<slug>[\w.@+-]+)/profile/detail/$', login_required(views.ProfileDetailView.as_view()), name='profile_detail'),
-        url(r'^profile/(?P<slug>[\w.@+-]+)/$', login_required(views.ProfileView.as_view()), name='profile_view'),
+        url(r'^$', views.Home.as_view(), name='home'), # ID 000
+
+        url(r'^accounts/', include('allauth.urls')), # IDs 081, 082, 083, etc
+        url(r'^admin/', include(admin.site.urls)), # ID 900
+        url(r'^blog/', include('zinnia.urls')), # IDs 050, 051, 052, etc
+        url(r'^comments/', include('django_comments.urls')), # für Blog
+        url(r'^impressum/$', views.Impressum.as_view(), name='impressum'), # ID 021
+        url(r'^warteliste/$', views.NewsletterSignup.as_view(), name='invite_me'), # ID 080
+        url(r'^protect/', include('simple_auth.urls')), # password-protection for staging-server
+        url(r'^question/', include('questions.urls')), # inaktiv, future feature
+        url(r'^summernote/', include('django_summernote.urls')), # für Blog
+        url(r'^zitat/', include('quotes.urls')), # ID 024
+
+        url(r'^startproject/$', login_required(views.StartProject.as_view()), name='startproject'), # ID 100
+        url(r'^project_overview/$', views.ProjectOverview.as_view(), name='project_overview'), # ID 101
+        url(r'^project/(?P<slug>[-\w]+)/$', login_required(views.ProjectDetailView.as_view()), name='project_detailview'), # ID 102
+        url(r'^project/(?P<slug>[-\w]+)/edit/$', login_required(views.ProjectUpdateView.as_view()), name='project_updateview'), # ID 103
+        
+        url(r'^profile/(?P<slug>[\w.@+-]+)/willkommen/$', login_required(views.WelcomePage.as_view()), name='welcome'), # ID 200
+        url(r'^profile/(?P<slug>[\w.@+-]+)/edit/$', login_required(views.ProfileUpdateView.as_view()), name='update_profile'), # ID 201
+        url(r'^profile/(?P<slug>[\w.@+-]+)/detail/$', login_required(views.ProfileDetailView.as_view()), name='profile_detail'), # ID 202
+        url(r'^profile/(?P<slug>[\w.@+-]+)/$', login_required(views.ProfileView.as_view()), name='profile_view'), # ID 203
         url(r'^profile/(?P<slug>[\w.@+-]+)/socialeebhaber/$', login_required(views.Socialeebhaber.as_view()), name='socialeebhaber'),
 
-        # Django CMS alsways at the end
-        url(r'^', include('cms.urls')),
+        # DjangoCMS, keep this always at the end
+        url(r'^', include('cms.urls')), 
     ]
 
 if not settings.DEBUG and not settings.SIMPLE_AUTH:
     urlpatterns = [
-        
-        url(r'^$', views.Home.as_view(), name='home'),
+
+        url(r'^$', views.Home.as_view(), name='home'), # ID 000
+
         url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
         url(r'^accounts/', include(allauth_urls.allauth_patterns)),
-        url(r'^admin/', include(admin.site.urls)),
-        # url(r'^summernote/', include('django_summernote.urls')),
-        # url(r'^question/', include('questions.urls')),
-        url(r'^quotes/', include('quotes.urls')),
-        url(r'^blog/', include('zinnia.urls')),
-        # url(r'^comments/', include('django_comments.urls')),
-        url(r'^invite_me/$', views.NewsletterSignup.as_view(), name='invite_me'),
-        url(r'^impressum/$', views.Impressum.as_view(), name='impressum'),
-        # url(r'^welcome/(?P<slug>[\w.@+-]+)/$', login_required(views.WelcomePage.as_view()), name='welcome'), # login required!
-        # url(r'^startproject/$', login_required(views.StartProject.as_view()), name='startproject'), # login required!
-        # url(r'^project_overview/$', views.ProjectOverview.as_view(), name='project_overview'),
-        # url(r'^project/(?P<slug>[-\w]+)/$', login_required(views.ProjectDetailView.as_view()), name='project_detailview'),
-        # url(r'^project/(?P<slug>[-\w]+)/edit/$', login_required(views.ProjectUpdateView.as_view()), name='project_updateview'),
-        # url(r'^(?P<slug>[\w.@+-]+)/profile/update/$', login_required(views.ProfileUpdateView.as_view()), name='update_profile'),
-        # url(r'^(?P<slug>[\w.@+-]+)/profile/detail/$', login_required(views.ProfileDetailView.as_view()), name='profile_detail'),
-        # url(r'^(?P<slug>[\w.@+-]+)/$', login_required(views.ProfileView.as_view()), name='profile_view'),
-        # url(r'^(?P<slug>[\w.@+-]+)/socialeebhaber/$', login_required(views.Socialeebhaber.as_view()), name='socialeebhaber'),
 
-        # Django CMS alsways at the end
+        # url(r'^accounts/', include('allauth.urls')), # IDs 081, 082, 083, etc
+        url(r'^admin/', include(admin.site.urls)), # ID 900
+        url(r'^blog/', include('zinnia.urls')), # IDs 050, 051, 052, etc
+        url(r'^comments/', include('django_comments.urls')), # für Blog
+        url(r'^impressum/$', views.Impressum.as_view(), name='impressum'), # ID 021
+        url(r'^warteliste/$', views.NewsletterSignup.as_view(), name='invite_me'), # ID 080
+        # url(r'^question/', include('questions.urls')), # inaktiv, future feature
+        url(r'^summernote/', include('django_summernote.urls')), # für Blog
+        url(r'^zitat/', include('quotes.urls')), # ID 024
+
+        # url(r'^startproject/$', login_required(views.StartProject.as_view()), name='startproject'), # ID 100
+        # url(r'^project_overview/$', views.ProjectOverview.as_view(), name='project_overview'), # ID 101
+        # url(r'^project/(?P<slug>[-\w]+)/$', login_required(views.ProjectDetailView.as_view()), name='project_detailview'), # ID 102
+        # url(r'^project/(?P<slug>[-\w]+)/edit/$', login_required(views.ProjectUpdateView.as_view()), name='project_updateview'), # ID 103
+        
+        # url(r'^profile/(?P<slug>[\w.@+-]+)/willkommen/$', login_required(views.WelcomePage.as_view()), name='welcome'), # ID 200
+        # url(r'^profile/(?P<slug>[\w.@+-]+)/edit/$', login_required(views.ProfileUpdateView.as_view()), name='update_profile'), # ID 201
+        # url(r'^profile/(?P<slug>[\w.@+-]+)/detail/$', login_required(views.ProfileDetailView.as_view()), name='profile_detail'), # ID 202
+        # url(r'^profile/(?P<slug>[\w.@+-]+)/$', login_required(views.ProfileView.as_view()), name='profile_view'), # ID 203
+        # url(r'^profile/(?P<slug>[\w.@+-]+)/socialeebhaber/$', login_required(views.Socialeebhaber.as_view()), name='socialeebhaber'),
+
+        # DjangoCMS, keep this always at the end
         url(r'^', include('cms.urls')),
+
     ]
 
 if settings.DEBUG:
