ca7d1f154b721bf098ccddb2b17dc94e0bb6e064
zee <zee@solarlamp.de>
2016-10-21 12:45:38 +0200

    added links to projects in comments

--- socialee/templatetags/socialee_filters.py
+++ socialee/templatetags/socialee_filters.py
@@ -1,6 +1,6 @@
 from django import template
 from django.template import Context, Template
-from socialee.models import User, Profile
+from socialee.models import CommonGround, Profile
 import re
 
 register = template.Library()
@@ -10,12 +10,13 @@ def link_ats(value):
 	c = Context()
 	for user in re.finditer(r'@(?P<slug>[\w.@+-]+)', value):
 		try:
-			u = User.objects.get(username=user.group(1))
+			u = CommonGround.objects.get(slug=user.group(1))
 			c[user.group(1)] = u
 		except:
 			pass
 	t = Template(re.sub(r'@(?P<slug>[\w.@+-]+)', 
-		r'{% if \1 %}<a href="{% url "profile_view" \1 %}">@\1</a>{% else %}@\1{% endif %}', value))
+		r'{% if \1.profile %}<a href="{% url "profile_view" slug=\1.slug %}">@\1</a>' +
+		r'{% elif \1.project %}<a href="{% url "project_view" slug=\1.slug %}">@\1</a>{% else %}@\1{% endif %}', value))
 	return t.render(c)
 
 @register.simple_tag
