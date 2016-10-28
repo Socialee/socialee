979a65c738e8ceac45a7a7ac63f1ec96a021647d
Moritz Juedes <mail@moritzjuedes.de>
2016-10-28 13:04:53 +0200

    Enable all Allauth-URLs

--- config/allauth_urls.py
+++ config/allauth_urls.py
@@ -3,15 +3,15 @@ from django.conf.urls import url
 from allauth.account import *
 
 allauth_patterns = [
-    # url(r"^signup/$", views.signup, name="account_signup"),
+    url(r"^signup/$", views.signup, name="account_signup"),
     url(r"^login/$", views.login, name="account_login"),
     url(r"^logout/$", views.logout, name="account_logout"),
 
-    # url(r"^password/change/$", views.password_change,
-    #     name="account_change_password"),
-    # url(r"^password/set/$", views.password_set, name="account_set_password"),
+    url(r"^password/change/$", views.password_change,
+        name="account_change_password"),
+    url(r"^password/set/$", views.password_set, name="account_set_password"),
 
-    # url(r"^inactive/$", views.account_inactive, name="account_inactive"),
+    url(r"^inactive/$", views.account_inactive, name="account_inactive"),
 
     # E-mail
     url(r"^email/$", views.email, name="account_email"),
@@ -21,13 +21,13 @@ allauth_patterns = [
         name="account_confirm_email"),
 
     # password reset
-    # url(r"^password/reset/$", views.password_reset,
-    #     name="account_reset_password"),
-    # url(r"^password/reset/done/$", views.password_reset_done,
-    #     name="account_reset_password_done"),
-    # url(r"^password/reset/key/(?P<uidb36>[0-9A-Za-z]+)-(?P<key>.+)/$",
-    #     views.password_reset_from_key,
-    #     name="account_reset_password_from_key"),
-    # url(r"^password/reset/key/done/$", views.password_reset_from_key_done,
-    #     name="account_reset_password_from_key_done"),
+    url(r"^password/reset/$", views.password_reset,
+        name="account_reset_password"),
+    url(r"^password/reset/done/$", views.password_reset_done,
+        name="account_reset_password_done"),
+    url(r"^password/reset/key/(?P<uidb36>[0-9A-Za-z]+)-(?P<key>.+)/$",
+        views.password_reset_from_key,
+        name="account_reset_password_from_key"),
+    url(r"^password/reset/key/done/$", views.password_reset_from_key_done,
+        name="account_reset_password_from_key_done"),
 ]
