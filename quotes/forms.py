d94d0c2f740ed52565bd9218f92155feef5eaec6
Moritz Juedes <mail@moritzjuedes.de>
2016-11-01 16:10:47 +0100

    Improve Zitate-App

--- quotes/forms.py
+++ quotes/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils.translation import ugettext_lazy as _
 
 from .models import Quote
 
@@ -7,4 +8,10 @@ class QuoteForm(forms.ModelForm):
     class Meta:
         model = Quote
         fields = ('title','author')
-    
+
+    def __init__(self, *args, **kwargs):
+        super(QuoteForm, self).__init__(*args, **kwargs)
+        self.fields['title'].widget.attrs['placeholder'] = "Ich habe fertig."
+        self.fields['title'].label = "Das Zitat:"
+        self.fields['author'].widget.attrs['placeholder'] = "Giovanni Trapattoni"
+        self.fields['author'].label = "Wer hat\'s gesagt?"
