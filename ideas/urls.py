dca44711af1ad5be2c6ed063295ef0a59fb8dbfa
Moritz Juedes <mail@moritzjuedes.de>
2016-11-01 11:45:16 +0100

    Make Ideen-App the frontpage (for trying yo)

--- ideas/urls.py
+++ ideas/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.CreateIdea.as_view(), name='create_idea'),
-    url(r'^galerie/$', views.idea_list, name='idea_list'),
+    url(r'^$', views.idea_list, name='idea_list'),
+    url(r'^neu/$', views.CreateIdea.as_view(), name='create_idea'),
     url(r'^like/$', views.Like.as_view(), name='like'),
 ]
