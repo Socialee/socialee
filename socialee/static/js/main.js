97cb95e53650a0f9484927cf5c2f4451ce366996
zee <zee@solarlamp.de>
2016-12-29 17:31:38 +0100

    update tabs to use foundation 6.3

--- socialee/static/js/main.js
+++ socialee/static/js/main.js
@@ -5,19 +5,6 @@ $(document).foundation();
 
 $(document).ready(function(){
 
-  $('a[href^="#"]').on('click',function (e) {
-	    e.preventDefault();
-
-	    var target = this.hash;
-	    var $target = $(target);
-
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 1200, 'swing', function () {
-	        window.location.hash = target;
-	    });
-	});
-
   $(".js-content-sized").each(fixSizedElements);
   $('.input_with_bound').each(setLimit);
   $('.input_with_bound').bind('input propertychange', setLimit);
